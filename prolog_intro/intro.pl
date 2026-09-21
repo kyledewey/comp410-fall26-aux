8006e743665a7405339767ca589ad53ca6c2f3fd
Kyle Dewey <kyle.dewey@csun.edu>
2026-09-21 15:45:56 -0700

    .

--- prolog_intro/intro.pl
+++ prolog_intro/intro.pl
@@ -12,6 +12,7 @@ foo(2, 4).
 
 % procedure - bar/4
 bar(a, b, 3, 8). % fact
+%bar(A, B, 3, 8).
 
 % procedure - isInteger/1
 %  name: isInteger
@@ -22,3 +23,52 @@ isInteger(3). % fact
 
 % alice is an atom (basically a string)
 isName(alice).
+
+% areEqual/2 procedure
+areEqual(SomeVariable, SomeVariable).
+%areEqual(_, 1).
+
+%% areEqual(0, 0).
+%% areEqual(1, 1).
+%% areEqual(2, 2).
+
+isWarm(pizza).
+isWarm(burger).
+isWarm(burrito).
+
+isCold(salad).
+
+isHot(coffee).
+
+foodTemp(coffee, hot).
+foodTemp(pizza, warm).
+foodTemp(burger, warm).
+foodTemp(burrito, warm).
+foodTemp(salad, cold).
+
+aliceLikes(pizza).
+aliceLikes(burger).
+aliceLikes(burrito).
+aliceLikes(yogurt).
+
+bobLikes(pizza).
+bobLikes(burger).
+bobLikes(salad).
+bobLikes(milk).
+
+isGood(alice, pizza).
+isGood(alice, burger).
+isGood(alice, burrito).
+isGood(alice, yogurt).
+isGood(bob, pizza).
+isGood(bob, burger).
+isGood(bob, salad).
+isGood(bob, milk).
+
+relation(alice, likes, burger).
+relation(alice, likes, burrito).
+relation(alice, hates, milk).
+
+likesPizzaAndBurger(Person) :-
+    isGood(Person, pizza),
+    isGood(Person, burger).
